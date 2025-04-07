fe58de673ff393cfe5e0bbaf0d23640aa773be14
Jenny Taranto <jennytaranto.pro@gmail.com>
2025-04-07 17:55:58 +0200

    corrections

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 const path = require('path');
 
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png'
+  };
+
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'pictures');
